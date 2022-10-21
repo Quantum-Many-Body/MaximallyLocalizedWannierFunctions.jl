c82bed40bdc44dd7a8a71b427efc81c74a98a711
Documenter.jl <documenter@juliadocs.github.io>
2022-10-21 05:49:19 +0000

    build based on a3f3985

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MaximallyLocalizedWannierFunctions","category":"page"},{"location":"#MaximallyLocalizedWannierFunctions","page":"Home","title":"MaximallyLocalizedWannierFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MaximallyLocalizedWannierFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MaximallyLocalizedWannierFunctions]","category":"page"}]
+}
